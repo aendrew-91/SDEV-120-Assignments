0caca8b9a5e3a1334853bd74b0fc3d0fcaf4532c
aendrew-91 <initialexists@gmail.com>
2025-02-24 12:54:45 -0600

    Add files via upload

---
+++ cats and dogs script - Andrew Escabi.py
@@ -0,0 +1,11 @@
+# Set variable c to "cats."
+c = "cats"
+
+# Set variable d to "dogs."
+d = "dogs"
+
+# Set variable s to the sentence to print
+s = f"It is raining {c} and {d}."
+
+# Print the sentence
+print(s)
